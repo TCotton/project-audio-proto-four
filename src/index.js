d4581c851c50d72c8916a4e4fd9818291e1e7efd
Andy Walpole <andywalpole@Andys-iMac.local>
2016-10-30 12:25:56 +0000

    added react router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {Router, browserHistory} from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -9,7 +10,7 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserHistory} />
+	</Provider>
+	, document.querySelector('.container'));
