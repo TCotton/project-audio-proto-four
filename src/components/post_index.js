df751d7b597847dd5ca0642bbc4613c2d4f84425
Andy Walpole <andywalpole@Andys-iMac.local>
2016-11-03 20:11:21 +0000

    added list of blog posts

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -10,15 +10,34 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+
+		if(!Array.isArray(this.props.posts)) {
+			return;
+		}
+
+		return this.props.posts.map((post) => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					<span className="pull-xs-right">{post.categories}</span>
+					<strong>{post.title}</strong>
+				</li>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div>
-					<div className="text-xs-right">
-						<Link to="/posts/new" className="btn btn-primary">
-							Add a post
-						</Link>
-					</div>
-					List of blog posts
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Add a post
+					</Link>
+				</div>
+				<h3>List of blog posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
@@ -29,4 +48,8 @@ function mapDispatchToProps(dispatch) {
 	return bindActionCreators({fetchPosts}, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(PostsIndex);
+function mapStateToProps(state) {
+	return {posts: state.posts.all};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
