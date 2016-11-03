4310e783c6cd267472d6d5ed84c3a04bfc242711
Andy Walpole <andywalpole@Andys-iMac.local>
2016-11-03 20:31:33 +0000

    added single get AJAX request

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=yEs4ahudUgE3ggg';
@@ -12,6 +13,19 @@ export function fetchPosts() {
 
 	return {
 		type: FETCH_POSTS,
+		payload: request
+	};
+
+}
+
+export function fetchPost(id) {
+
+	console.log('run this');
+
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+	return {
+		type: FETCH_POST,
 		payload: request
 	};
 
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class PostsShow extends Component {
+
+	componentWillMount() {
+		this.props.fetchPost(this.props.params.id);
+	}
 
 	render() {
 		return <div>Show posts {this.props.params.id}</div>;
@@ -8,4 +15,8 @@ class PostsShow extends Component {
 
 }
 
-export default PostsShow;
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({fetchPost}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsShow);
